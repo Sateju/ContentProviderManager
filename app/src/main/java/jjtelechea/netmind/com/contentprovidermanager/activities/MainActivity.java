d25ae0bbf6680435929ba294b2a4b2c3e7c33401
Sateju <juan40ts@gmail.com>
2016-11-28 10:52:18 +0100

    ParamsDb and SQLiteOpenHelper aggregated

--- app/src/main/java/jjtelechea/netmind/com/contentprovidermanager/activities/MainActivity.java
+++ app/src/main/java/jjtelechea/netmind/com/contentprovidermanager/activities/MainActivity.java
@@ -1,9 +1,11 @@
-package jjtelechea.netmind.com.contentprovidermanager;
+package jjtelechea.netmind.com.contentprovidermanager.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import jjtelechea.netmind.com.contentprovidermanager.R;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -25,7 +27,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View view) {
 
-        switch (view.getId()){
+        switch (view.getId()) {
 
             case R.id.btnQueryDB:
 
